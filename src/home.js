abd07ef4207e7018983bf81bafdcf1dc061c92f8
RohithYelagam <rohithyalagam2001@gmail.com>
2021-05-03 14:29:54 +0530

    dp5

--- src/home.js
+++ src/home.js
@@ -7,17 +7,17 @@ export default function Home() {
     const [news ,setNews] = useState([]);
 
     useEffect(() => {
-        // axios.get('https://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=cd4f72e2c6084071aab8911a8b2643a5').then((res)=>{
-        //     setNews(res.data.articles);
-           
-        // })
-        axios.get('http://api.mediastack.com/v1/news?access_key=ab627f3dbb236c631e5c3da1adf55dec&categories=health&languages=en&limit=50').then((res)=>{
-            // console.log("response from home");
-            // console.log(res.data.data);&countries=in
-            setNews(res.data.data);
+        axios.get('https://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=cd4f72e2c6084071aab8911a8b2643a5').then((res)=>{
+            setNews(res.data.articles);
            
         })
-        console.log(news);
+        // axios.get('http://api.mediastack.com/v1/news?access_key=ab627f3dbb236c631e5c3da1adf55dec&categories=health&languages=en&limit=50').then((res)=>{
+        //     // console.log("response from home");
+        //     // console.log(res.data.data);&countries=in
+        //     setNews(res.data.data);
+           
+        // })
+        // console.log(news);
     }, [])
 
     return (
@@ -29,11 +29,11 @@ export default function Home() {
                             news.map((a)=>(
                                 
                                 <div className="news_container">
-                                    {a.image!==null?(<div className="news">
-                                        <div className="photo"><a href={a.url} target="blank"><img src={a.image} width="160px" height="100px"></img></a></div>
+                                    {a.urlToImage!==null?(<div className="news">
+                                        <div className="photo"><a href={a.url} target="blank"><img src={a.urlToImage} width="160px" height="100px"></img></a></div>
                                 <div classNam="news_text">
                                 <div className="title"><a href={a.url} target="blank">{a.title}</a></div>
-                                <div className="author">{a.source!=null?(<div>source: {a.source}</div>):(<div></div>)}</div></div>
+                                <div className="author">{a.source.name!=null?(<div>source: {a.source.name}</div>):(<div></div>)}</div></div>
                                     </div>):(
                                             <div>
                                                 
