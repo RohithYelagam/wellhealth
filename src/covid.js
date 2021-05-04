ab7e4f9136a65e6f0358d07a9fab8e4774e3288c
RohithYelagam <rohithyalagam2001@gmail.com>
2021-05-04 15:10:13 +0530

    dp6

--- src/covid.js
+++ src/covid.js
@@ -2,23 +2,28 @@ import React, { useState, useEffect } from "react";
 import Chart from "react-google-charts";
 import axios from "axios";
 import "./covid.css";
-import { CountryDropdown, RegionDropdown } from 'react-indian-state-region-selector';
-
+import {
+  CountryDropdown,
+  RegionDropdown,
+} from "react-indian-state-region-selector";
 
 export default function Covid() {
   const [a, setA] = useState();
   const [c, setC] = useState();
   const [r, setR] = useState();
   const [d, setD] = useState();
-  const [arr,setArr] = useState();
- const [country,setCountry] = useState('');
- const [obj,setObj] = useState({});
+  const [arr, setArr] = useState();
+  const [country, setCountry] = useState("");
+  const [obj, setObj] = useState({});
+  const [rows,setRows] = useState([]);
 
- const selectCountry = (val) => {
+  const selectCountry = (val) => {
     setCountry(val);
-    let ob = arr.find(o => o.state === val);
+    let ob = arr.find((o) => o.state === val);
     setObj(ob);
-  }
+  };
+
+
 
   useEffect(() => {
     axios.get("https://api.covid19india.org/data.json").then((res) => {
@@ -28,13 +33,33 @@ export default function Covid() {
       setD(res.data.statewise[0].deaths);
       setArr(res.data.cases_time_series);
     });
-    axios.get("https://api.covid19india.org/data.json").then((res)=>{
-        setArr(res.data.statewise);
-    })
-    
+    axios.get("https://api.covid19india.org/data.json").then((res) => {
+      setArr(res.data.statewise);
+      var dd ="";
+      res.data.cases_time_series.map((x)=>{
+        dd=x.dateymd;
+        var p = new Date();
+        p.setFullYear(Number(dd.substring(0,4)),Number(dd.substring(5,7))-1,Number(dd.substring(8,10)));
+        rows.push([p,Number(x.totalconfirmed)]);
+      })
+
+    });
+
     
   }, []);
+  
 
+  const columns = [
+    {
+      type:'date',
+      label:"Date"
+    },
+    {
+      type: "number",
+      label: "Total Confirmed Caeses"
+    },
+  ];
+  
 
   return (
     <div className="covid">
@@ -43,7 +68,6 @@ export default function Covid() {
       </div>
 
       <div className="total_india">
-           
         <div className="confirmed">
           <h3>Confirmed</h3>
           <div className="c_cases">
@@ -71,21 +95,54 @@ export default function Covid() {
       </div>
 
       <div className="graphs">
+      <Chart
+        width={"500px"}
+        height={"300px"}
+        chartType="AreaChart"
+        loader={<div>Loading Chart</div>}
        
-        </div>
+        rows={rows}
+          columns={columns}
+        options={{
+         title:"Total Confirmed Cases Timeline in India",
+         titleTextStyle:{
+          color:'#3366CC',
+          fontSize:16,
+          bold:true,
+         },
+          hAxis: {
+          gridlines: {
+            color: 'transparent'
+        }},
+        // legendTextStyle: { color: 'red' },
+          vAxis: { minValue: 0,
+            gridlines: {
+              color: 'transparent'
+          } },
+          chartArea: { width: "80%", height: "80%"},
+          backgroundColor: {
+            fill: '#151D33',
+          },
+          height:400,
+          width:800
+        }}
+        // For tests
+        rootProps={{ "data-testid": "1" }}
+      />
+      </div>
 
-
-        <div >
-            <CountryDropdown className="drop_down"
+      <div>
+        <CountryDropdown
+          className="drop_down"
           value={country}
-          onChange={(val) => selectCountry(val)} />
-        </div>
+          onChange={(val) => selectCountry(val)}
+        />
+      </div>
 
-            <div className="country">
-            <h2>{country}</h2>
-            </div>
-           
-        
+      <div className="country">
+        <h2>{country}</h2>
+      </div>
+
       <div className="total_india">
         <div className="confirmed">
           <h3>Confirmed</h3>
@@ -112,9 +169,8 @@ export default function Covid() {
           </div>
         </div>
       </div>
+
       
-      
-      
-      </div>
+    </div>
   );
 }
